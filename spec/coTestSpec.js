9a46724ab300a2dca1310ed4f2a367287c9aa8af
Ruben Osmar Alvarado <rubenosmaralvarado@MacBook-Air-de-Ruben.local>
2021-10-17 18:50:18 -0500

    split classes in different files and make the index for 30 days report. Fix all eslint issues

---
+++ spec/coTestSpec.js
@@ -0,0 +1,13 @@
+/* eslint-disable prefer-arrow-callback */
+const { expect } = require('chai');
+
+const Product = require('../src/product');
+const CarInsurance = require('../src/carInsurance');
+
+describe('Product test suite', function suite() {
+  it('should foo', function foo() {
+    const coTest = new CarInsurance([new Product('foo', 0, 0)]);
+    const products = coTest.updatePrice();
+    expect(products[0].name).equal('foo');
+  });
+});
